e49099065952257f00946d20cd9d067b1e318c6c
AM <AM@AM.lv>
2017-02-09 14:45:36 +0200

    Initial Create

---
+++ src/main/java/lv/demo/cv/CountryCodeValidatorApplication.java
@@ -0,0 +1,12 @@
+package lv.demo.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryCodeValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryCodeValidatorApplication.class, args);
+	}
+}
